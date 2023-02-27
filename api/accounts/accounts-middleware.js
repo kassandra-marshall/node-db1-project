c7102e0b592e70936dfb81c2dce37a5fd90afbaa
dyzn3y <kdmarshall728@gmail.com>
2023-02-27 16:22:05 -0500

    wrote out all end points and got all tests passing

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -4,31 +4,30 @@ exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
-  if(!req.body.name || !req.body.budget){
-    next({ status: 400, message: 'name and budget are required' })
-    // ||  ||
-    
-    // 
-    
+  if(req.body.name === undefined || req.body.budget === undefined){
+    next({ status: 400, message: 'name and budget are required' })    
   } else if (req.body.name.trim().length < 3 || req.body.name.trim().length > 100){
-      next({ message: 'name of account must be between 3 and 100' })
+      next({ status: 400, message: 'name of account must be between 3 and 100' })
   } else if (!parseInt(req.body.budget)){
-    next({ message: 'budget of account must be a number' })
+    next({ status: 400, message: 'budget of account must be a number' })
   } else if (parseInt(req.body.budget) < 0 || parseInt(req.body.budget) > 1000000){
-    next({ message: 'budget of account is too large or too small' })
-  }
-  else {
+    next({ status: 400, message: 'budget of account is too large or too small' })
+  } else {
     next()
   }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
-  const account = await db('accounts').where('name', req.body.name).first()
-  if (account){
-    next({ status: 400, message: 'that name is taken' })
-  } else {
-    next()
+  try {
+    const existing = await db('accounts').where('name', req.body.name).first()
+    if(existing){
+      next({ status: 400, message: 'that name is taken' })
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
   }
 
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,68 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model');
+const { checkAccountId, checkAccountNameUnique, checkAccountPayload } = require('./accounts-middleware')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const accounts = await Accounts.getAll()
+    res.json(accounts)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const account = await Accounts.getById(req.params.id)
+    res.json(account)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    const { name } = req.body;
+    req.body.name = name.trim();
+    const account = await Accounts.create(req.body)
+    res.status(201).json(account)
+    
+  } catch (error) {
+    next(error)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.put('/:id', checkAccountId, checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const { name } = req.body;
+    req.body.name = name.trim();
+    const account = await Accounts.updateById(req.params.id, req.body)
+    res.json(account)
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.delete('/:id', checkAccountId, async (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+    const account = await Accounts.deleteById(req.params.id)
+    res.json(account);
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 module.exports = router;
