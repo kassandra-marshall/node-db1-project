916f93c0a6a49e17aa8a4a8a53a544c2605083aa
dyzn3y <kdmarshall728@gmail.com>
2023-02-27 14:28:38 -0500

    wrote middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,44 @@
+const db = require('../../data/db-config');
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+  if(!req.body.name || !req.body.budget){
+    next({ status: 400, message: 'name and budget are required' })
+    // ||  ||
+    
+    // 
+    
+  } else if (req.body.name.trim().length < 3 || req.body.name.trim().length > 100){
+      next({ message: 'name of account must be between 3 and 100' })
+  } else if (!parseInt(req.body.budget)){
+    next({ message: 'budget of account must be a number' })
+  } else if (parseInt(req.body.budget) < 0 || parseInt(req.body.budget) > 1000000){
+    next({ message: 'budget of account is too large or too small' })
+  }
+  else {
+    next()
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
+exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
+  const account = await db('accounts').where('name', req.body.name).first()
+  if (account){
+    next({ status: 400, message: 'that name is taken' })
+  } else {
+    next()
+  }
+
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  const account = await db('accounts').where('id', req.params.id).first()
+  if (!account){
+    next({ status: 404, message: 'account not found'})
+  } else {
+    next()
+  }
 }
